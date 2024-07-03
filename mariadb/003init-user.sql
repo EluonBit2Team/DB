78dbb20e76e8ba30cf6d353db2413889532fdf16
gyujun9403 <gujun112@naver.com>
2024-07-03 16:31:43 +0900

    style:db언어 설정

--- mariadb/003init-user.sql
+++ mariadb/003init-user.sql
@@ -1,4 +1,8 @@
 CREATE USER 'today_chicken'@'%' IDENTIFIED BY '1q2w3e4r';
 GRANT ALL PRIVILEGES ON *.* TO 'today_chicken'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'today_chicken'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 'grafana_dash'@'%' IDENTIFIED BY '1q2w3e4r';
+GRANT SELECT ON `server_statistic_db`.`statistic` TO 'grafana_dash'@'%';
+GRANT SELECT ON `server_statistic_db`.`statistic` TO 'grafana_dash'@'localhost';
 FLUSH PRIVILEGES;
