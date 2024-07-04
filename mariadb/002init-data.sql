01d8d541899c02bea36a51ea6e67e9e814defb97
gyujun9403 <gujun112@naver.com>
2024-07-04 09:12:48 +0900

    bug:유저 초기 값 변경

--- mariadb/002init-data.sql
+++ mariadb/002init-data.sql
@@ -1,9 +1,9 @@
 use user_setting_db;
 INSERT INTO user (login_id, password, name, phone, email, did, position, role, create_date, max_tps)
-    VALUES ('admin', UNHEX(SHA2('admin', 256)), 'eluon', '010', 'gmail', 0, 0, 1, NOW(), 120);
+    VALUES ('admin', UNHEX(SHA2('admin', 256)), 'eluon', '010', 'gmail', 1, 1, 1, NOW(), 120);
 
 INSERT INTO user (login_id, password, name, phone, email, did, position, role, create_date, max_tps)
-    VALUES ('admin2', UNHEX(SHA2('admin2', 256)), 'eluon', '010', 'gmail', 0, 0, 1, NOW(), 120);
+    VALUES ('admin2', UNHEX(SHA2('admin2', 256)), 'eluon', '010', 'gmail', 1, 1, 1, NOW(), 120);
 
 INSERT INTO user_setting_db.dept (did, dept_name)
     VALUES (1, '인사');
