630adb66d6e4c9e83d55814143a06f3951a8e4f6
root <root@EluonYGJ>
2024-07-08 13:27:24 +0900

    feat:유저 생성 수정

--- mariadb/003init-user.sql
+++ mariadb/003init-user.sql
@@ -12,15 +12,15 @@
 CREATE USER IF NOT EXISTS 'today_chicken'@'%' IDENTIFIED BY '1q2w3e4r';
 GRANT ALL PRIVILEGES ON *.* TO 'today_chicken'@'%' WITH GRANT OPTION;
 
--- CREATE USER IF NOT EXISTS 'today_chicken'@'localhost' IDENTIFIED BY '1q2w3e4r';
--- GRANT ALL PRIVILEGES ON *.* TO 'today_chicken'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'today_chicken'@'localhost' IDENTIFIED BY '1q2w3e4r';
+GRANT ALL PRIVILEGES ON *.* TO 'today_chicken'@'localhost' WITH GRANT OPTION;
 
 -- Create and grant privileges to 'grafana_dash'
 CREATE USER IF NOT EXISTS 'grafana_dash'@'%' IDENTIFIED BY '1q2w3e4r';
 GRANT SELECT ON `server_statistic_db`.`statistic` TO 'grafana_dash'@'%';
 
--- CREATE USER IF NOT EXISTS 'grafana_dash'@'localhost' IDENTIFIED BY '1q2w3e4r';
--- GRANT SELECT ON `server_statistic_db`.`statistic` TO 'grafana_dash'@'localhost';
+CREATE USER IF NOT EXISTS 'grafana_dash'@'localhost' IDENTIFIED BY '1q2w3e4r';
+GRANT SELECT ON `server_statistic_db`.`statistic` TO 'grafana_dash'@'localhost';
 
 -- Apply changes
 FLUSH PRIVILEGES;
