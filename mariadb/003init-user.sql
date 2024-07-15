153cef7546f9dc948663941f6d227d469be78758
root <root@EluonYGJ>
2024-07-15 15:22:21 +0900

    feat: 그라파나계정 권한 추가

--- mariadb/003init-user.sql
+++ mariadb/003init-user.sql
@@ -6,5 +6,10 @@ GRANT ALL PRIVILEGES ON *.* TO 'today_chicken'@'localhost' WITH GRANT OPTION;
 
 CREATE USER IF NOT EXISTS 'grafana_dash'@'%' IDENTIFIED BY '1q2w3e4r';
 GRANT SELECT ON `server_statistic_db`.`statistic` TO 'grafana_dash'@'%';
+GRANT SELECT ON `log_db`.`server_log` TO 'grafana_dash'@'%';
+
+CREATE USER IF NOT EXISTS 'grafana_dash'@'%' IDENTIFIED BY '1q2w3e4r';
+GRANT SELECT ON `server_statistic_db`.`statistic` TO 'grafana_dash'@'localhost';
+GRANT SELECT ON `log_db`.`server_log` TO 'grafana_dash'@'localhost';
 
 FLUSH PRIVILEGES;
